5aef503e3f5ae8e3299d912307b30a3f2a344c20
Giovanni Cutrì <89142084+giovanni-cutri@users.noreply.github.com>
2023-05-27 14:57:47 +0200

    Get lyrics of songs

--- script.R
+++ script.R
@@ -1,36 +1,49 @@
 library(geniusr)
 library(tidyverse)
 library(tidytext)
+library(quanteda)
 
 genius_token()
 
 artist_id <- search_artist("Caparezza")$artist_id
-artist_songs <- get_artist_songs(artist_id = artist_id)
+artist_songs <- get_artist_songs(artist_id)
 
-songs_id <- c()
+songs_ids <- c()
 for (song in artist_songs$content) {
   song_id <- song$id
-  songs_id <- songs_id %>% append(song_id)
+  songs_ids <- songs_ids %>% append(song_id)
 }
 
-song_titles <- c()
-song_albums <- c()
-for (song_id in songs_id) {
-  song <- get_song(song_id)$content
+songs_titles <- c()
+songs_albums <- c()
+songs_lyrics <- c()
+for (i in 1:length(songs_ids)) {
+  cat("Getting", i, "of", length(songs_ids), "id:", songs_ids[i], "\n")
+  song <- get_song(songs_ids[i])$content
   song_title <- song$title
   song_album <- song$album$name
+  song_lyrics <- list(get_lyrics_id(songs_ids[i]))
   # some songs have no album associated with them, so we won't consider them
   if (!is.null(song_album)){
-    song_titles <- song_titles %>% append(song_title)
-    song_albums <- song_albums %>% append(song_album)
+    songs_titles <- songs_titles %>% append(song_title)
+    songs_albums <- songs_albums %>% append(song_album)
+    songs_lyrics <- songs_lyrics %>% append(song_lyrics) 
   }
 }
 
-songs <- data.frame(title = song_titles, album = song_albums)
-songs %>% head()
+# collapse all lyrics lines into a single text
+for (i in 1:length(songs_lyrics)){
+  songs_lyrics[[i]] <- songs_lyrics[[i]]$line %>% paste(collapse = " ")
+}
+
+songs_lyrics <- unlist(songs_lyrics)
+
+songs <- data.frame(title = songs_titles, album = songs_albums, lyrics = songs_lyrics)
+songs %>% head(1)
 
 # we are going to ignore alternative versions of the same song, such as
 # live versions, radio edits and remixes
 # we keep "demo" versions because they are sufficiently different from the final
 # version, knowing Caparezza's discography
-test <- songs %>% filter(!grepl("Live|Radio Edit|Radio Version|Remix|RMX", title))
+songs <- songs %>% filter(!grepl("Live|Radio Edit|Radio Version|Remix|RMX", title))
+
